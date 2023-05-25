935d7db9dcfe35192797c7f3efa9960697324cb5
Nandar Sukmana <nsukmana.dev@gmail.com>
2023-05-25 11:07:01 +0700

    migration

--- database/migrations/2023_05_25_033659_create_m501_users_table.php
+++ database/migrations/2023_05_25_033659_create_m501_users_table.php
@@ -15,7 +15,29 @@ return new class extends Migration
     {
         Schema::create('m501_users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('username', 50);
+            $table->string('password');
+            $table->string('erp_user_id', 50);
+            $table->integer('roles_id');
+            $table->integer('organization_id');
+            $table->boolean('is_tmm');
+            $table->boolean('is_login_mobile');
+            $table->string('fcm_token');
+            $table->string('nik', 15);
+            $table->string('full_name', 150);
+            $table->string('email', 100)->unique();
+            $table->string('phone', 50);
+            $table->string('image_url');
+            $table->string('bank_name', 50);
+            $table->string('bank_account', 50);
+            $table->boolean('has_npwp');
+            $table->string('address');
+            $table->boolean('is_active');
+            $table->rememberToken();
+            $table->string('created_by', 100);
+            $table->timestamp('created_on')->useCurrent();
+            $table->string('updated_by', 100);
+            $table->timestamp('updated_on')->useCurrentOnUpdate();
         });
     }
 
