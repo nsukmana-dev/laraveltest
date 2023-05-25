c2d02b358fccaed498539c579aa90a87ca34c603
Nandar Sukmana <nsukmana.dev@gmail.com>
2023-05-25 10:40:47 +0700

    create controller, model and migration

---
+++ app/Http/Controllers/M501UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\m501_user;
+use Illuminate\Http\Request;
+
+class M501UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\m501_user  $m501_user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(m501_user $m501_user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\m501_user  $m501_user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(m501_user $m501_user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\m501_user  $m501_user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, m501_user $m501_user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\m501_user  $m501_user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(m501_user $m501_user)
+    {
+        //
+    }
+}
